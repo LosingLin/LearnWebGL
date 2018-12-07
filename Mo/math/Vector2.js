8bbcfc552779ee89b24568362fdebfedc1b0967f
LosingLin <764837522@qq.com>
2018-12-07 18:57:30 +0800

    Update Vector2.js

--- Mo/math/Vector2.js
+++ Mo/math/Vector2.js
@@ -7,40 +7,57 @@ function Vector2( x, y ) {
 }
 
 Object.defineProperties( Vector2.prototype, {
-
 	width: {
-
 		get () {
-
 			return this.x;
-
 		},
-
 		set ( value ) {
-
 			this.x = value;
-
 		}
 
 	},
-
 	height: {
-
 		get: function () {
-
 			return this.y;
-
 		},
-
 		set: function ( value ) {
-
 			this.y = value;
-
 		}
 
 	}
 
 } );
+
+Object.assign( Vector2.prototype, {
+	isVector2: true,
+	set: function( x, y ) {
+		this.x = x;
+		this.y = y;
+		return this;
+	},
+	
+	add: function(v) {
+		this.x += v.x;
+		this.y += v.y;
+		return this;
+	},
+	sub: function(v) {
+		this.x -= v.x;
+		this.y -= v.y;
+		return this;
+	},
+	multiply: function(v) {
+		this.x *= v.x;
+		this.y *= v.y;
+		return this;
+	},
+	divide: function(v) {
+		this.x /= v.x;
+		this.y /= v.y;
+	},
+} );
+
+
 
 // exports = Vector2;
 // module.exports = Vector2;
